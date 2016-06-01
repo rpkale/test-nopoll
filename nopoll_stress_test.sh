bddfce6d74659b8502ac7bc4613a3c870c64636a
Rahul Kale <Rahul.Kale@barco.com>
2016-06-01 14:37:00 -0700

    Added README file and stress test script

---
+++ nopoll_stress_test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+IPADDRESS=$1
+SERVER_PORT=$2
+DURATION=$3
+SPAWNCOUNT=$4
+for count in $(seq 1 ${SPAWNCOUNT}) ; do
+    ./test_nopoll client ${count} ${IPADDRESS} ${SERVER_PORT} ${DURATION} &
+done
+
+echo "Waiting for processes to finish..."
+wait
+
+
+
+
+
+
+
+
+
+
+
+
+
